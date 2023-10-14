26eb4c727a5039dc60edb93128c3a417262950d2
Trần Thanh An <37788933+thanhan1997tr@users.noreply.github.com>
2023-10-14 20:14:53 +0700

    init projects

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+     content: ["./layout/*.liquid", "./sections/*.liquid", "./snippets/*.liquid"],
+     theme: {
+          extend: {},
+     },
+     plugins: [],
+};
